8a638de128224f8ef580a4e411a0deee57e17517
Markus Schulz <schulz@alpharesearch.de>
2019-09-15 13:56:44 -0400

    remove not used

--- ICantBelieveItsNotValetudo/run.sh
+++
@@ -1,7 +0,0 @@
-echo Setup config
-echo '{"mqtt":' > /app/config.json
-cat /data/options.json >> /app/config.json
-echo ', "webserver": { "enabled": true, "port": 3000 }}' >> /app/config.json
-#cat /app/config.json
-
-cd /app && npm start
--- MaryTTS/run.sh
+++
@@ -1,8 +0,0 @@
-echo Setup language packs
-
-# Get specified language packs
-packs=$(cat '/data/options.json' | jq '.languagePacks')
-
-
-echo Start MaryTTS
-cd '/app' && bin/marytts-server
